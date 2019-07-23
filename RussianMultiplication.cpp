7e603eb61a7cc7c8af89390fbe13db3049725aff
Sankarsh Pallela <34985173+sankarsh98@users.noreply.github.com>
2019-07-23 12:03:10 +0530

    Add files via upload

---
+++ RussianMultiplication.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int pr(int a,int b){
+    int p=b,mu=0;
+    for(int i=1;a;i++){
+        if(a%2!=0) mu+=p;
+        p=p*2;
+        a/=2;
+    }
+    return mu;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<pr(a,b);
+    return 0;
+}
