7fb06f7b2f8373e5f93fcd345d18e74675441e7e
Sankarsh Pallela <34985173+sankarsh98@users.noreply.github.com>
2019-07-23 11:44:25 +0530

    Add files via upload

---
+++ Distance.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+float dis(int a,int b,int c,int d){
+    return sqrt(pow((b-a),2)+pow((d-c),2));
+}
+int main()
+{
+    float x1,x2,y1,y2;
+    cin>>x1>>x2>>y1>>y2;
+    printf("%.2f",dis(x1,y1,x2,y2));
+    return 0;
+}
